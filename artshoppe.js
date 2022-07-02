943317d8a8bcb8221acd942ca37a6b6ddb7c6f4f
Steve Wakhungu <stwakhungu@gmail.com>
2022-07-02 21:42:25 +0300

    Add form submit event listener

--- artshoppe.js
+++ artshoppe.js
@@ -1,3 +1,9 @@
 let change = () =>{
     document.getElementById("press").style.backgroundColor = "blue";
 };
+
+
+document.getElementById("myForm").onsubmit = function() {myFunction()};
+function myFunction() {
+        alert("Your form was submitted successfully. Thank you!")
+}
