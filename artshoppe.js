0ec5be02f6a06413c7f416eefc9c213a86f2d6c0
Steve Wakhungu <stwakhungu@gmail.com>
2022-07-04 00:37:21 +0300

    Chaanges to page layout using HTML and CSS primarily

--- artshoppe.js
+++ artshoppe.js
@@ -6,17 +6,22 @@ function mOut(obj) {
 }
 
 let change = () =>{
-    document.getElementById("press").style.backgroundColor = "blue";
+    document.getElementById("press").style.backgroundColor = "red";
 };
 
-function submit(event) {
-    log.textContent = `Form successfully submitted! Time stamp: ${event.timeStamp}`
-    event.preventDefault();
-}
-const form = document.getElementById("myForm");
-const log = document.getElementById("log")
-form.addEventListener("submit", logSubmit);
+// function submit(event) {
+//     log.textContent = `Form successfully submitted! Time stamp: ${event.timeStamp}`
+//     event.preventDefault();
+// }
+// const form = document.getElementById("myForm");
+// const log = document.getElementById("log")
+// form.addEventListener("submit", logSubmit);
 // document.getElementById("myForm").onsubmit = function() {myFunction()};
 // function myFunction() {
 //         alert("Your form was submitted successfully. Thank you!")
 // }
+const form = document.getElementById("myForm") 
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+    log.textContent = `Form successfully submitted! Time stamp: ${event.timeStamp}`
+})
